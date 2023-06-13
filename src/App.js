9ea54dadc045668d15eb3784351af0647910c8c9
nrimsky <ninarimsky@gmail.com>
2023-06-13 21:15:29 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -3,62 +3,98 @@ import Card from './components/Card';
 import './App.css'
 import { generateDeck, generateRule, isCorrectMatch } from './lib/helpers';
 
+const MAX_ROUNDS = 64;
+const CONSECUTIVE_CORRECT_ANSWERS = 10;
 
 function App() {
   const [cards, setCards] = useState([]);
   const [rule, setRule] = useState();
-  const [results, setResults] = useState([{
-    nCorrect: 0,
-    nIncorrect: 0,
-    timeElapsed: 0,
-  }]);
+  const [results, setResults] = useState([]);
+  const [refIdx, setRefIdx] = useState(0);
   const [startTime, setStartTime] = useState(null);
   const [message, setMessage] = useState('   ');
+  const [roundsCompleted, setRoundsCompleted] = useState(0);
 
   useEffect(() => {
+    startNewRound();
+  }, []);
+
+  const startNewRound = () => {
     const newRule = generateRule();
     setRule(newRule);
     const cards = generateDeck(newRule);
     setCards(cards);
     setStartTime(Date.now()); // Set the start time when a new rule is generated
-  }, []);
+    setRefIdx(results.length);
+  }
 
   useEffect(() => {
-    const lastResult = results[results.length - 1];
-    if (lastResult.nCorrect >= 10) {
-      const newRule = generateRule();
-      setRule(newRule);
-      const cards = generateDeck(newRule);
-      setCards(cards);
-
-      // Get the end time of the round
-      const endTime = Date.now();
-
-      // Calculate time elapsed in seconds
-      const timeElapsed = (endTime - startTime) / 1000;
-
-      // Push the timeElapsed to the lastResult and start a new round
-      setResults(prevResults => {
-        const newResults = [...prevResults];
-        newResults[newResults.length - 1].timeElapsed = timeElapsed;
-        return [...newResults, {
-          nCorrect: 0,
-          nIncorrect: 0,
-          timeElapsed: null,
-        }];
-      });
-
-      // Set the start time for the new round
-      setStartTime(Date.now());
+    if (results.length - refIdx >= 10) {
+      const last10 = results.slice(results.length - CONSECUTIVE_CORRECT_ANSWERS, results.length);
+      // Were all correct?
+      if (last10.every(result => result.isCorrect)) {
+        startNewRound();
+        setRoundsCompleted(prevRoundsCompleted => prevRoundsCompleted + 1);
+      }
     }
-  }, [results]);
+  }, [results, refIdx]);
 
   useEffect(() => {
     // Fade message after 2 seconds
-    setTimeout(() => {
+    const messageTimer = setTimeout(() => {
       setMessage('   ');
     }, 1000);
+
+    return () => {
+      clearTimeout(messageTimer);
+    };
   }, [message]);
+
+  const handleCardClick = (card) => {
+
+    const isCorrectMatchCard = isCorrectMatch(card, cards[cards.length - 1], rule);
+    const timeElapsed = Date.now() - startTime;
+
+    setResults(prevResults => {
+      const newResults = [...prevResults, {
+        isCorrect: isCorrectMatchCard,
+        timeElapsed: timeElapsed/1000,
+        rule: rule,
+      }];
+      return newResults;
+    });
+
+    setMessage(isCorrectMatchCard ? 'Correct!' : 'Incorrect!');
+
+    const newDeck = generateDeck(rule);
+    setCards(newDeck);
+
+    setStartTime(Date.now());
+  };
+
+  useEffect(() => {
+    // dvnk maps to 0123 for the 4 cards
+    // we have onPress listeners for keys d v n k
+    const handleKeyPress = (event) => {
+      const key = event.key;
+      if (key === 'd') {
+        handleCardClick(cards[0]);
+      } else if (key === 'v') {
+        handleCardClick(cards[1]);
+      } else if (key === 'n') {
+        handleCardClick(cards[2]);
+      } else if (key === 'k') {
+        handleCardClick(cards[3]);
+      }
+    };
+
+    document.addEventListener('keypress', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keypress', handleKeyPress);
+    };
+  }, [cards]);
+
 
   const exportResults = () => {
     const blob = new Blob([JSON.stringify(results, null, 2)], { type: 'application/json' });
@@ -71,33 +107,23 @@ function App() {
     document.body.removeChild(link);
   }
 
+  if (roundsCompleted >= MAX_ROUNDS) {
+    return (
+      <div className='game'>
+        <h1>Game Finished</h1>
+        <p>All rounds completed. You can no longer click around.</p>
+        <button className='export' onClick={exportResults}>Export results</button>
+      </div>
+    );
+  }
+
   return (
     <div className='game'>
       <h1>Card matching exercise</h1>
       <p>Click on the top card that matches the bottom card</p>
       <div className='top-cards'>
         {cards.slice(0, 4).map((card, index) => (
-          <Card {...card} id={index} isSelectable={true} key={index} onClick={() => {
-            const isCorrectMatchCard = isCorrectMatch(card, cards[cards.length - 1], rule);
-            console.log(isCorrectMatchCard, rule);
-            if (isCorrectMatchCard) {
-              setResults(prevResults => {
-                const newResults = [...prevResults];
-                newResults[newResults.length - 1].nCorrect++;
-                return newResults;
-              });
-              setMessage('Correct!');
-            } else {
-              setResults(prevResults => {
-                const newResults = [...prevResults];
-                newResults[newResults.length - 1].nIncorrect++;
-                return newResults;
-              });
-              setMessage('Incorrect!');
-            }
-            const newDeck = generateDeck(rule);
-            setCards(newDeck);
-          }} />
+          <Card {...card} id={index} isSelectable={true} key={index} onClick={() => handleCardClick(card)} />
         ))}
       </div>
       <div className='bottom-card'>
